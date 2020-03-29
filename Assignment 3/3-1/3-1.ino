0cc688eb245494b89d267feaa54e6bfddce76fca
Peratorn Boonyient <bimai32882@hotmail.com>
2020-03-29 22:19:54 +0700

    3-2

--- Assignment 3/3-1/3-1.ino
+++ Assignment 3/3-1/3-1.ino
@@ -1,7 +1,21 @@
-void setup(){
 
-}
+void setup(){
+    pinMode(2, INPUT_PULLUP);
+    }
 
 void loop(){
     
+    if (digitalRead(2) == 0) {
+    int inMin = 3;
+    int inMax = 13;
+    for(int i=inMin; i<=inMax; i++)
+    digitalWrite(i, 1) ;
+    delay(500) ;
+    {
+    int inMin = 3;
+    int inMax = 13;
+    for(int i=inMin; i<=inMax; i++)
+    digitalWrite(i, 0) ;    
+    }
+    }
 }
